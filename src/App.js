3f0891278de4a3a1b8f9a91813ac343db201017d
karamat ali <karamat.4101@gmail.com>
2024-04-16 19:36:13 +0500

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div class='flex items-center justify-center min-h-screen'>
+    <div class='w-full max-w-xl px-6 py-5 mx-auto bg-white rounded-lg shadow-md  ring-2 ring-gray-300 ring-inset'>
+      <div className="w-full flex flex-row justify-between py-4 ">
+        <p className="text-xl font-['Poppins']">All pages</p>
+        <input type="checkbox" className="form-checkbox h-6 w-6 text-indigo-600  rounded-sm"/>
+
+      </div>
+      <hr className="my-2 border border-gray-300"/>
+      <div className="w-full flex flex-row justify-between py-4 ">
+        <p className="text-xl font-['Poppins']">Page 1</p>
+        <input type="checkbox" className="form-checkbox h-6 w-6 text-indigo-600  rounded-sm"/>
+      </div>
+      <div className="w-full flex flex-row justify-between py-4 ">
+        <p className="text-xl font-['Poppins']">Page 2</p>
+        <input type="checkbox" className="form-checkbox h-6 w-6 text-indigo-600  rounded-full"/>
+      </div>
+      <div className="w-full flex flex-row justify-between py-4 ">
+        <p className="text-xl font-['Poppins']">Page 3</p>
+        <input type="checkbox" className="form-checkbox h-6 w-6 text-indigo-600 "/>
+      </div>
+      <hr className="my-2  border border-gray-300"/>
+      <button className="w-full py-2 mt-4 text-center bg-yellow-400">
+        Done
+      </button>
     </div>
+</div>
   );
 }
 
